47a65d347abd78e3e7df9dec756a8ba7e1fdf36d
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 18:33:53 -0400

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
